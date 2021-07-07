44d53ee90ad76e7a95d373bb3cfae95c03ae2d6d
ntphi <phinguyen202@gmail.com>
2021-07-07 23:28:48 +0700

    Run .mei file on local

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Synth } from '../node_modules/tone/build/esm/instrument/Synth';
 import verovio from 'verovio';
+import mei from '../../svg-sheet-music/dev/sample/doc_starts_with_music.mei';
 
 class RootTest extends React.Component<any, any> {
     constructor(props: any) {
@@ -11,12 +12,12 @@ class RootTest extends React.Component<any, any> {
         verovio.module.onRuntimeInitialized = function () {
             /* create the toolkit instance */
             var vrvToolkit = new verovio.toolkit();
-            fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
-                .then((response) => response.text())
-                .then((meiXML) => {
-                    let svg = vrvToolkit.renderData(meiXML, {});
+            // fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
+            //     .then((response) => response.text())
+            //     .then((meiXML) => {
+                    let svg = vrvToolkit.renderData(mei, {});
                     document.getElementById("notation").innerHTML = svg;
-                });
+                // });
         }
     }
 
@@ -32,7 +33,7 @@ class RootTest extends React.Component<any, any> {
             <div>
                 <button onClick={this.test}>play audio</button>
             </div>
-            <div id="notation "></div>
+            <div id="notation"></div>
             </>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+
+      {
+        test: /\.mei$/i,
+        use: 'raw-loader',
+      },
     ]
   },
   stats: {
